4330173b631bc50866b27688c517c21c10a6ecf4
gidskql6671 <gidskql6671@gmail.com>
2021-07-12 23:02:17 +0900

    변경된 코드에 맞게 명령 수정

--- run.sh
+++ run.sh
@@ -9,9 +9,8 @@ make
 python3 src/make_mats.py img_mats/outs_img2mat/
 
 # run the filter program
-# TODO: img_mats/outs_apply_filter/out_apply_filter.dat -> img_mats/outs_apply_filter/ 에 맞게 코드 수정
-c_executions/main img_mats/outs_img2mat/ img_mats/outs_apply_filter/out_apply_filter.dat < configs/filter.txt
+c_executions/main img_mats/outs_img2mat/ img_mats/outs_apply_filter/ < configs/filter.txt
 
 # run python program to generate images from matrix
 # output written as 0.bmp, 1.bmp ...
-python3 src/load_img.py img_mats/outs_apply_filter/out_apply_filter.dat result_images
+python3 src/load_img.py img_mats/outs_apply_filter/ result_images/
