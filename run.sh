986967515d213a0742cf82d90e1f03e75202c18d
gidskql6671 <gidskql6671@gmail.com>
2021-07-12 16:18:00 +0900

    디렉토리명 변경에 따른 수정

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ make
 python3 src/make_mats.py img_mats/out.dat
 
 # run the filter program
-build/main img_mats/out.dat img_mats/out_apply_filter.dat < configs/filter.txt
+c_executions/main img_mats/out.dat img_mats/out_apply_filter.dat < configs/filter.txt
 
 # run python program to generate images from matrix
 # output written as 0.bmp, 1.bmp ...
