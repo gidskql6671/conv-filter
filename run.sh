18a34c007915b1e3f148cab8551c356b18a029be
gidskql6671 <gidskql6671@gmail.com>
2021-07-12 23:30:36 +0900

    변경된 프로그램에 맞게 수정 매개변수로 코어의 개수를 받게함

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+num_cores=5
+
 # clean the current build
 make clean
 make
 
 # make image matrices and store them 
-# Usage: python3 make_mats.py outs_dir/ [num_cores(default : 1)]
-python3 src/make_mats.py img_mats/outs_img2mat/
+# Usage: python3 make_mats.py outs_dir/ num_cores
+python3 src/make_mats.py img_mats/outs_img2mat/ $num_cores
 
 # run the filter program
-c_executions/main img_mats/outs_img2mat/ img_mats/outs_apply_filter/ < configs/filter.txt
+c_executions/main img_mats/outs_img2mat/ img_mats/outs_apply_filter/ $num_cores < configs/filter.txt
 
 # run python program to generate images from matrix
-# output written as 0.bmp, 1.bmp ...
-python3 src/load_img.py img_mats/outs_apply_filter/ result_images/
+# output written as batch0/0.bmp, batch0/1.bmp, ..., batch2/0.bmp, batch2/1.bmp ...
+python3 src/load_img.py img_mats/outs_apply_filter/ result_images/ $num_cores
