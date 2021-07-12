1614ced68cf4de6d18eb0441f7e20e4238a3a547
gidskql6671 <gidskql6671@gmail.com>
2021-07-12 23:01:20 +0900

    변경된 인풋/아웃풋을 반영함
    input/output 디렉토리를 명령행 인자로 받음.
    input 디렉토리에 있는 .dat 파일에 대해 작업을 진행.
    output 디렉토리에는 input 파일 개수만큼 output 파일을 만들어줌

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -210,3 +211,149 @@ int main(int argc, char *argv[]) {
   return 0; 
 }
 >>>>>>> 2b70e56 (임시 코드 변경)
+=======
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <vector>
+#include "filter.hpp"
+#include "conv2d_layer.hpp"
+#include <experimental/filesystem>
+
+using namespace std;
+namespace fs = std::experimental::filesystem;
+
+
+int w_size, bias;     // kernel window size
+double** kernel; // image kernel 
+vector<filter*> filters;
+
+void push_filter(int idx) {     
+  // make edge detector for color idx 
+  double ***ed = get_tensor(w_size, w_size, 3); 
+  for (int i = 0; i < w_size; ++i) {
+    for (int j = 0; j < w_size; ++j) {
+      ed[i][j][idx] = kernel[i][j];
+    }
+  }
+  filter *f = new filter(ed, w_size, 3, bias);
+  f->normalize();
+  filters.push_back(f);
+}
+
+void applyfilter2matrix(ifstream& ifile, ofstream& ofile){
+
+  int width, num_images, height, depth;
+  int stride = 1, padding = 1; 
+  ifile >> num_images >> width >> height >> depth;
+  ofile << num_images << " "; 
+  cerr << num_images << " "; 
+
+  
+  conv_layer clayer(width, height, depth, w_size, stride, padding, filters.size()); 
+  double ***input;
+  input = get_tensor(width, height, depth); 
+  
+  for (int id = 0; id < num_images; ++id) {
+
+    // read one image
+    for (int i = 0; i < width; ++i) {
+      for (int j = 0; j < height; ++j) {
+        for (int k = 0; k < depth; ++k) {
+          ifile >> input[i][j][k];
+          if (ifile.peek() == ',') ifile.ignore();
+        }
+      }
+    }
+    auto output = clayer.conv2d(input, filters);
+    double ***out_volume = get<3>(output);
+    int o_width = get<0>(output), o_height = get<1>(output), o_depth = get<2>(output);
+    if (id == 0) {
+      // print image dimensions only the first time
+      ofile << o_width << " " << o_height << " " << o_depth << "\n";
+      cerr << o_width << " " << o_height << " " << o_depth << "\n";
+    }
+
+    for (int i = 0; i < o_width; ++i) {
+      for (int j = 0; j < o_height; ++j) {
+        ofile << out_volume[i][j][0] << "," << out_volume[i][j][1] 
+                << "," << out_volume[i][j][2] << " ";      
+      }
+      ofile << "\n";
+    }
+    ofile << "\n";
+  }
+  ifile.close();
+  ofile.close();
+}
+
+
+int main(int argc, char *argv[]) {
+
+  //  input the kernel matrix
+  cin >> w_size >> bias;
+  kernel = new double*[w_size]; 
+  for (int i = 0; i < w_size; i++) {
+    kernel[i] = new double[w_size];
+    for (int j = 0; j < w_size; j++) {
+      cin >> kernel[i][j]; 
+    }
+  }
+
+  push_filter(0); // R
+  push_filter(1); // G
+  push_filter(2); // B
+  if (argc < 3) {
+    cerr << "usage <input_data filder name> <output folder name>" << endl;
+    return 1;
+  }
+  
+  const fs::path inputFoler = argv[1];
+  const fs::path outputFoler = argv[2];
+  
+  vector<ifstream*> ifiles;
+  vector<ofstream*> ofiles;
+  
+  // inputFolder, outputFolder가 없으면 만들어줌.
+  if (!fs::exists(inputFoler))
+    fs::create_directory(inputFoler);
+  if (!fs::exists(outputFoler))
+    fs::create_directory(outputFoler);
+    
+    
+  // inputFolder의 파일과 stream을 연결해서 벡터에 넣어줌.
+  for (auto& inputFilePath : fs::directory_iterator(inputFoler)){
+    if (inputFilePath.path().extension() == ".dat")
+      ifiles.push_back(new ifstream(inputFilePath.path().c_str()));
+      
+      if (!ifiles.back()->is_open()){
+        cerr << "Unable to open " << inputFilePath << endl;
+        return 1;
+      }
+  }
+  // input 파일의 개수만큼 output 파일을 만들어줌.
+  for (int i = 0; i < ifiles.size(); i++){
+    string outputFilename = "out_apply_filter" + to_string(i) + ".dat";
+    const fs::path outputFilepath = outputFoler / outputFilename;
+    
+    ofiles.push_back(new ofstream(outputFilepath.c_str()));
+    
+    if (!ofiles.back()->is_open()){
+      cerr << "Unable to open " << outputFilepath << endl;
+      return 1;
+    }
+  }
+  
+  // TODO: 병렬 프로세싱 적용
+  for(int i = 0; i < ifiles.size(); i++){
+    ifstream& ifile = *(ifiles[i]);
+    ofstream& ofile = *(ofiles[i]);
+    
+    applyfilter2matrix(ifile, ofile);
+  }
+
+  return 0; 
+}
+>>>>>>> ffbc958 (변경된 인풋/아웃풋을 반영함)
