ae77704b33f9d4d2ee76ccf96bfc2eaea383c6e8
gidskql6671 <gidskql6671@gmail.com>
2021-07-12 19:19:45 +0900

    아웃풋 파일 경로 변경

--- run.sh
+++ run.sh
@@ -9,10 +9,9 @@ make
 python3 src/make_mats.py img_mats/outs_img2mat/
 
 # run the filter program
-# TODO: img_mats/out.dat -> img_mats/outs_img2mat/ 에 맞게 코드 수정해야함.
-# TODO: img_mats/out_apply_filter.dat -> img_mats/outs_apply_filter/ 에 맞게 코드 수정
-c_executions/main img_mats/out.dat img_mats/out_apply_filter.dat < configs/filter.txt
+# TODO: img_mats/outs_apply_filter/out_apply_filter.dat -> img_mats/outs_apply_filter/ 에 맞게 코드 수정
+c_executions/main img_mats/outs_img2mat/ img_mats/outs_apply_filter/out_apply_filter.dat < configs/filter.txt
 
 # run python program to generate images from matrix
 # output written as 0.bmp, 1.bmp ...
-python3 src/load_img.py img_mats/out_apply_filter.dat result_images
+python3 src/load_img.py img_mats/outs_apply_filter/out_apply_filter.dat result_images
